f0281c04522676cecedb5f197c86f1a8ec10d7c5
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-10-03 16:13:25 +0900

    Find or create task

--- backend/app/controllers/api/v1/tasks_controller.rb
+++ backend/app/controllers/api/v1/tasks_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class TasksController < ApplicationController
       def index
-        render json: { title: 'zenn-app-title' }
+        task = Task.find_or_create_by(title: 'Good Task')
+        render json: { title: task.title }
       end
     end
   end
