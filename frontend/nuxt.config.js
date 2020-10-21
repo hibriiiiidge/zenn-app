183417664b21093b7705bfb4e53dfc2ac127fdf3
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-10-21 12:49:24 +0900

    Remove unecessary config about babel

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -52,13 +52,4 @@ export default {
   axios: {
     baseURL: process.env.NODE_ENV === "production" ? "https://backend.hibriiiiidge.com" : "http://localhost:5000"
   },
-  babel: {
-    presets: [
-      'es2015',
-      'stage-0',
-    ],
-    plugins: [
-      'transform-runtime'
-    ]
-  },
 }
